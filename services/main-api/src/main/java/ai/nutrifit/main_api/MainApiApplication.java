ac8ec4cb153b13e6754a4110dd79ca2a7162a671
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-04 20:55:43 +0300

    initialize main-api Spring Boot project

---
+++ services/main-api/src/main/java/ai/nutrifit/main_api/MainApiApplication.java
@@ -0,0 +1,13 @@
+package ai.nutrifit.main_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( MainApiApplication.class, args);
+	}
+
+}
---
+++ services/main-api/src/test/java/ai/nutrifit/main_api/MainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.nutrifit.main_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
