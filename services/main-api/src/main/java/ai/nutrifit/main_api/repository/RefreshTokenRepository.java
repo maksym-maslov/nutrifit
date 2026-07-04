b44d3be32020d854b8e3217ef3744dbeefdf0cb1
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-04 20:57:15 +0300

    Add RefreshTokenRepository and UserRepository interfaces for managing refresh tokens and users in the database

---
+++ services/main-api/src/main/java/ai/nutrifit/main_api/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package ai.nutrifit.main_api.repository;
+
+import ai.nutrifit.main_api.entity.RefreshToken;
+import ai.nutrifit.main_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+}
