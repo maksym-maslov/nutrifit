4f39c2cc4a9ba4e66d44fd4a282174109715e16f
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-07 12:38:53 +0300

    Add dependency for tests

--- services/main-api/build.gradle.kts
+++ services/main-api/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-jdbc-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-security-oauth2-resource-server-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
