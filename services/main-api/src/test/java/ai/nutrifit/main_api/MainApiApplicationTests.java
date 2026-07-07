42a798c66965d6262350a159ab0172ebf64091a7
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-07 12:38:23 +0300

    Update main class of tests

--- services/main-api/src/test/java/ai/nutrifit/main_api/MainApiApplicationTests.java
+++ services/main-api/src/test/java/ai/nutrifit/main_api/MainApiApplicationTests.java
@@ -1,9 +1,11 @@
 package ai.nutrifit.main_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MainApiApplicationTests {
 
 	@Test
