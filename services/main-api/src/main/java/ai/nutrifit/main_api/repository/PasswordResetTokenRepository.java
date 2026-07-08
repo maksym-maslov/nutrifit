8654c52bbf4988f3e06788cc3659b924a607af43
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-08 12:50:12 +0300

    Add password reset token repository

---
+++ services/main-api/src/main/java/ai/nutrifit/main_api/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,13 @@
+package ai.nutrifit.main_api.repository;
+
+import ai.nutrifit.main_api.entity.PasswordResetToken;
+import ai.nutrifit.main_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+
+    void deleteByUser(User user);
+}
