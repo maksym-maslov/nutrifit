5d43766e67b36ab84ad14b765e0ed9e6b9fa6c5a
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-08 14:26:21 +0300

    Add rate limit endpoints

---
+++ services/main-api/src/main/java/ai/nutrifit/main_api/shared/ratelimit/RateLimitEndpoint.java
@@ -0,0 +1,10 @@
+package ai.nutrifit.main_api.shared.ratelimit;
+
+public enum RateLimitEndpoint {
+    AUTH_LOGIN,
+    AUTH_REGISTER,
+    AUTH_REFRESH,
+    AUTH_CHANGE_PASSWORD,
+    API_SEARCH,
+    API_RECOMMEND
+}
