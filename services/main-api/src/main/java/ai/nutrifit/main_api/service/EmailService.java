7126db874971d44bddad842c85524ba438a9368a
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-08 12:51:36 +0300

    Add new method to email service and its implementation

--- services/main-api/src/main/java/ai/nutrifit/main_api/service/EmailService.java
+++ services/main-api/src/main/java/ai/nutrifit/main_api/service/EmailService.java
@@ -2,4 +2,6 @@ package ai.nutrifit.main_api.service;
 
 public interface EmailService {
     void sendVerificationEmail(String to, String token);
+
+    void sendPasswordResetEmail(String to, String token);
 }
