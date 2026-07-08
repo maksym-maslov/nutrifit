5fe0a786ccea6f6c8d39692374cbc9d7090fbf49
maksym-maslov <maslov.maksym.serhiiovych@gmail.com>
2026-07-08 14:58:41 +0300

    Add deps for testing

--- services/main-api/build.gradle.kts
+++ services/main-api/build.gradle.kts
@@ -36,6 +36,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter:1.21.3")
+	testImplementation("org.testcontainers:postgresql:1.21.3")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
